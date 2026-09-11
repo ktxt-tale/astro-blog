b1b583b3a34cd8ebff463b0202692aafb151f1c4
ktxt-tale <kiemtienxaitet123@gmail.com>
2026-09-11 23:41:37 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://ktxt-tale.github.io',
-  base: '/astro-blog',
+  integrations: [react()],
 });
